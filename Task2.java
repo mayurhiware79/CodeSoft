8ce888605661f10e6db88c6ed18d8ed4c341cf42
Mayur Hiware <157942713+mayurhiware79@users.noreply.github.com>
2024-08-02 19:18:02 +0530

    Update and rename StudentGradeCalculator.java to Task2.java

--- Task2.java
+++ Task2.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-public class StudentGradeCalculator {
+//StudentGradeCalculator
+public class Task2 {
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
